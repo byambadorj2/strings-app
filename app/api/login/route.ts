3951e013a583c2046cb9401d712f28250cb1e0a8
Byambadorj <byambadorj993@gmail.com>
2024-04-19 21:01:05 +0800

    token

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,10 @@
 import { sql } from "@/db";
+import { tr } from "@faker-js/faker";
+import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
+
 import { NextResponse } from "next/server";
+import { use } from "react";
 
 export async function POST(request: Request) {
   const json = await request.json();
@@ -7,5 +12,25 @@ export async function POST(request: Request) {
     "select id, username, password from users where username ilike $1",
     [json.username]
   );
-  return NextResponse.json({ data: res.rows[0] });
+  if (res.rowCount == 0) {
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  }
+  const user = res.rows[0];
+  const match = await bcrypt.compare(json.password, user.password);
+  if (!match) {
+    return NextResponse.json({ error: "invilad credential" }, { status: 401 });
+  }
+  const token = await new SignJWT({})
+    .setProtectedHeader({ alg: "HS256" })
+    .setSubject(user.id)
+    .setIssuedAt()
+    .setExpirationTime("2w")
+    .sign(new TextEncoder().encode("my-jwt-secret"));
+  const response = NextResponse.json({ msg: "login success" });
+  response.cookies.set("jwk-token", token, {
+    sameSite: "strict",
+    httpOnly: true,
+    secure: true,
+  });
+  return response;
 }

8250bf5887ba5c0386acf5b1e7d592eaf7c2419a
Byambadorj <byambadorj993@gmail.com>
2024-04-20 16:22:34 +0800

    added

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     [json.username]
   );
 
-  if (res.rowCount > 0) {
+  if (res.rowCount! > 0) {
     return NextResponse.json({ error: "user already exists" }, { status: 400 });
   }
   const saltRounds = 10;

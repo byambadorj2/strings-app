6c5a115835804ffc2ddcd12c5e576285422488fe
Byambadorj <byambadorj993@gmail.com>
2024-04-21 14:43:17 +0800

    users profile api endpoint

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -23,9 +23,9 @@ export async function POST(request: Request) {
     .setSubject(user.id)
     .setIssuedAt()
     .setExpirationTime("2w")
-    .sign(new TextEncoder().encode("my-jwt-secret"));
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
   const response = NextResponse.json({ msg: "login success" });
-  response.cookies.set("jwk-token", token, {
+  response.cookies.set("jwt-token", token, {
     sameSite: "strict",
     httpOnly: true,
     secure: true,

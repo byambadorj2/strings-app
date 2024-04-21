3d924195ea7b82ec3452977f67d1fb97db498c82
Byambadorj <byambadorj993@gmail.com>
2024-04-21 16:39:48 +0800

    avatar. user components

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,4 +1,5 @@
 import useSWR from "swr";
+import User from "../components/user";
 
 export default function Header() {
   const { data, error, isLoading } = useSWR("/api/users/profile");
@@ -6,5 +7,14 @@ export default function Header() {
   if (isLoading) return <div>loading...</div>;
 
   console.log(data);
-  return <header>{data.data.username}</header>;
+  return (
+    <header className="flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justify-between items-center">
+      <div>
+        <h1 className="font-mono text-lg">Strings</h1>
+      </div>
+      <div>
+        <User user={data.data} href="account" />
+      </div>
+    </header>
+  );
 }

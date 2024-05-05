6fd09f5e0207c560e20697071560f872f2148df3
Byambadorj <byambadorj993@gmail.com>
2024-05-05 14:36:02 +0800

    delete post endpoint

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,5 +1,6 @@
 import { getJWTPayload } from "@/app/util/auth";
 import { sql } from "@/db";
+import { error } from "console";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -36,4 +37,20 @@ export async function PATCH(
     params.id,
   ]);
   return NextResponse.json({ msg: "update success" });
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: number } }
+) {
+  const jwtPayload = await getJWTPayload();
+  const res = await sql("delete from posts where user_id = $1 and id = $2", [
+    jwtPayload.sub,
+    params.id,
+  ]);
+  if (res.rowCount == 1) {
+    return NextResponse.json({ msg: "delete success" });
+  } else {
+    return NextResponse.json({ error: "not found" }, { status: 404 });
+  }
 }

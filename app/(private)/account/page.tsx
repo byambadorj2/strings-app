fe0c3cba07921686270466614fc032162cde5891
mobicom-byambadorj-ganbold <byambadorj.ganbold@mobicom.mn>
2024-05-11 13:54:18 +0800

    logout, account page

---
+++ app/(private)/account/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import AvatarForm from "./avatar-form";
+import SignOutButton from "./signout-btn";
+
+export default function AccountPage() {
+  return (
+    <div>
+      <h2> Account Page</h2>
+      <AvatarForm />
+      <SignOutButton />
+    </div>
+  );
+}
---
+++ app/api/logout/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const response = NextResponse.json({ msg: "logout success" });
+  response.cookies.set("jwt-token", "");
+  return response;
+}

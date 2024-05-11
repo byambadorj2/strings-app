35f6f8689cf5b8620267e41b7f60ae9da1b95064
mobicom-byambadorj-ganbold <byambadorj.ganbold@mobicom.mn>
2024-05-11 16:05:08 +0800

    user page

---
+++ app/(private)/[username]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import PostContainer from "@/app/components/post-container";
+import UserPageHeader from "./user-page-header";
+
+export default function UserPage({ params }: { params: { username: string } }) {
+  return (
+    <div>
+      <UserPageHeader username={params.username} />
+      <PostContainer username={params.username} />
+    </div>
+  );
+}

94dff8184b094566b2e2afd81ac7b493e5461d15
mobicom-byambadorj-ganbold <byambadorj.ganbold@mobicom.mn>
2024-05-13 19:45:11 +0800

    delete btn

--- app/(private)/profile/edit-post/[id]/page.tsx
+++ app/(private)/profile/edit-post/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useSWR from "swr";
 import Form from "./form";
+import DeleteBtn from "./delete-btn";
 
 export default function EditPost({ params }: { params: { id: number } }) {
   const { data, error, isLoading } = useSWR("/api/posts/" + params.id);
@@ -12,7 +13,7 @@ export default function EditPost({ params }: { params: { id: number } }) {
       <h2>Edit Post</h2>
       <div className="flex flex-col gap-10">
         <Form post={data.data} />
-        {/* <DeleteBtn /> */}
+        <DeleteBtn post={data.data} />
       </div>
     </div>
   );
